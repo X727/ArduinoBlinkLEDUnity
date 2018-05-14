529dff1ef7ada2699c821f8e446ced19bcc4f897
X727 <patrick.ignoto@mail.mcgill.ca>
2018-05-14 17:19:40 -0400

    Initial Commit
    Initial Commit of test for Ready Makers. Unity scene of button that
    will control LED on Arduino in the future. Includes on-click method
    that writes debug message on click, based on its state.

---
+++ Arduino Blink LED Project/Assets/ButtonFunctionality.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFunctionality : MonoBehaviour {
+
+	public bool state;
+
+	// Use this for initialization
+	void Start () {
+		state = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ButtonClickFunction () {
+
+		state = !state;
+
+		if (state) {
+			Debug.Log ("Button clicked. LED On.");
+		} else {
+			Debug.Log ("Button clicked. LED Off.");
+		}
+
+	}
+
+
+}
