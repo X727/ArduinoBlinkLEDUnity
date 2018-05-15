507499698ee69e4b3a95cd0419d46e9fcf642b7b
X727 <patrick.ignoto@mail.mcgill.ca>
2018-05-15 09:10:51 -0400

    Started adding Serial port features and changed .NET version
    Started adding serial port functions to script and fixed a bug where it
    was not recognizing System.IO.Ports because it was using a subset of
    the .NET framework.

--- Arduino Blink LED Project/Assets/ButtonFunctionality.cs
+++ Arduino Blink LED Project/Assets/ButtonFunctionality.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.IO.Ports;
 using UnityEngine;
 
 public class ButtonFunctionality : MonoBehaviour {
 
 	public bool state;
+	static SerialPort _serialPort;
 
 	// Use this for initialization
 	void Start () {
 		state = false;
+		string port = "";
+		int baudRate = 0;
+		_serialPort = new SerialPort(port, baudRate);
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 
 	public void ButtonClickFunction () {
